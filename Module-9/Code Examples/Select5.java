757cf22c9a14fc693cf8607e4e327db6c0b68585
Yohannes Ayele <yojoayele@gmail.com>
2025-05-05 01:31:14 -0400

    Update Module 9

---
+++ Module-9/Code Examples/Select5.java
@@ -0,0 +1,50 @@
+/*
+ *
+ * Professor Darrell Payne
+ * Bellevue University
+ *
+ */
+import java.sql.*;
+
+public class Select5{
+
+  public static void main(String args[]){
+
+    try{
+
+      Connection con;
+
+      Class.forName("com.mysql.cj.jdbc.Driver");
+
+      String url = "jdbc:mysql://localhost:3306/databasedb?";
+
+      con = DriverManager.getConnection(url + "user=student1&password=pass");
+
+      System.out.println("Connection established - now executing a select");
+
+      Statement stmt = con.createStatement();
+      ResultSet rs = stmt.executeQuery("SELECT * FROM address33");
+
+     System.out.println("Received Results:");
+
+     int i = rs.getMetaData().getColumnCount();
+
+     while(rs.next()){
+
+       for(int x = 1; x <= i; ++x){
+
+         System.out.println(rs.getString(x));
+       }
+
+       System.out.println("");
+     }
+
+     stmt.close();
+     con.close();
+    }
+    catch (java.lang.Exception ex){
+
+      ex.printStackTrace();
+    }
+  }
+}
